93a17607011a20b17c4c7666f558ae4d04249d67
tanmaymestry <tanmaymestry34@gmail.com>
2024-05-09 23:07:55 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return a list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n", " ") for req in requirements]
+setup (
+name='MLPROJECT',
+version='0.0.1',
+author='Tanmay Mestry',
+author_email='tanmaymestry34@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirement.txt')
+)
